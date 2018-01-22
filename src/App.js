9ef57b761bbc3841ff5358a043ef66c89b0edad2
Chongyu Ren <rchongyu@gmail.com>
2018-01-21 21:10:25 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ClueCheater from './components/ClueCheater.js'
+
+class App extends Component {
+  render() {
+    return (
+      <ClueCheater />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ClueCheater.js
@@ -0,0 +1,11 @@
+import React, { Component }from "react"
+
+class ClueCheater extends Component {
+    render() {
+        return (
+            <input />
+        );
+    }
+}
+
+export default ClueCheater
